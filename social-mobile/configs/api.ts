931d6c814b6b50445f11c6b149166f0b75dfb2e6
Ei Maung <eimg@fairwayweb.com>
2026-03-07 09:51:53 +0630

    next movie

--- social-mobile/configs/api.ts
+++ social-mobile/configs/api.ts
@@ -1,4 +1,4 @@
 import { Platform } from "react-native";
 
 export const apiURL =
-	Platform.OS == "ios" ? "http://localhost:8800" : "http://10.0.2.2:8800";
+	Platform.OS == "android" ? "http://10.0.2.2:8800" : "http://localhost:8800";
